0559224d957fee59d11c9c006afd5ffb717a096d
emanueledb <emanueledibene@gmail.com>
2022-09-29 19:02:35 +0200

    dependencies updated

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
-import "bootstrap-vue/dist/bootstrap-vue.css"
-import "bootstrap/dist/css/bootstrap.css"
 import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { faCheck, faTrashCan, faEdit } from "@fortawesome/free-solid-svg-icons"
 import { faCircleXmark } from "@fortawesome/free-regular-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import "bootstrap/dist/css/bootstrap.css"
 
 library.add(faTrashCan, faCheck, faCircleXmark, faEdit)
 Vue.use(BootstrapVue)
